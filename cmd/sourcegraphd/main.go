7668bc6eae44cf3a807815cf56fc1330125449d0
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2023-06-13 13:27:22 -0400

    deploy/installer: Add k3s reset for upgrade process

--- cmd/sourcegraphd/main.go
+++ cmd/sourcegraphd/main.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/journald"
+	"github.com/sourcegraph/deploy/internal/k3s"
 	"github.com/sourcegraph/deploy/internal/sourcegraph"
+	"github.com/sourcegraph/deploy/internal/system/service"
 )
 
 var (
@@ -29,6 +31,37 @@ func main() {
 func run(ctx context.Context, logger *zerolog.Logger) error {
 	logger.Info().Str("version", version).Str("sgversion", sgversion).Msg("starting sourcegraphd")
 
+	logger.Info().Msg("running update check process")
+	update, err := sourcegraph.CheckUpdate()
+	if err != nil {
+		logger.Error().Err(err).Msg("failed to run update check process")
+		return err
+	}
+
+	if update {
+		logger.Info().Msg("updated needed, running update process")
+
+		// TODO kill k3s, clean dirs, restart
+		logger.Info().Msg("resetting k3s")
+		err := k3s.Reset(ctx)
+		if err != nil {
+			logger.Error().Err(err).Msg("failed to reset k3s")
+			return err
+		}
+
+		err = service.Start(ctx, "k3s.service")
+		if err != nil {
+			logger.Error().Err(err).Msg("failed to reset k3s")
+			return err
+		}
+
+		err = sourcegraph.HelmUpgrade()
+		if err != nil {
+			logger.Error().Err(err).Msg("failed to run update process")
+			return err
+		}
+	}
+
 	logger.Info().Msg("check for existing sourcegraph installation")
 	installed, err := sourcegraph.IsInstalled("sourcegraph")
 	if err != nil {
@@ -46,22 +79,6 @@ func run(ctx context.Context, logger *zerolog.Logger) error {
 	}
 
 	logger.Info().Msg("sourcegraph installed")
-
-	logger.Info().Msg("running update check process")
-	update, err := sourcegraph.CheckUpdate()
-	if err != nil {
-		logger.Error().Err(err).Msg("failed to run update check process")
-		return err
-	}
-
-	if update {
-		logger.Info().Msg("updated needed, running update process")
-		err := sourcegraph.HelmUpgrade()
-		if err != nil {
-			logger.Error().Err(err).Msg("failed to run update process")
-			return err
-		}
-	}
 
 	return nil
 }
---
+++ internal/k3s/reset.go
@@ -0,0 +1,47 @@
+package k3s
+
+import (
+	"context"
+	"os"
+	"os/exec"
+)
+
+// Reset will stop and reset the state of the k3s installation.
+func Reset(ctx context.Context) error {
+	err := killall(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = cleanup()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// killall stops all k3s containers and resets the containerd state
+func killall(ctx context.Context) error {
+	err := exec.CommandContext(ctx, "/usr/local/bin/k3s-killall.sh").Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// cleanup removes left over TLS certs and creds that will cause k3s to not reboot on a new system
+func cleanup() error {
+	err := os.RemoveAll("/var/lib/rancher/k3s/server/cred")
+	if err != nil {
+		return err
+	}
+
+	err = os.RemoveAll("/var/lib/rancher/k3s/server/tls")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
