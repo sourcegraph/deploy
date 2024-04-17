1bb7d1502b8dfba6355d2b8490d060fcf1559e69
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2024-04-17 16:57:52 -0400

    Bump k3s to version 1.29.3+k3s1

--- packer/gcp/init.sh
+++ packer/gcp/init.sh
@@ -49,7 +49,7 @@ sudo sh -c "echo '* hard nofile 262144' >> /etc/security/limits.conf"
 ###############################################################################
 # Install k3s (Kubernetes single-machine deployment)
 ###############################################################################
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.29.0+k3s1 K3S_TOKEN=none sh -s - \
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.29.3+k3s1 K3S_TOKEN=none sh -s - \
     --node-name sourcegraph-0 \
     --write-kubeconfig-mode 644 \
     --cluster-cidr 10.10.0.0/16 \
