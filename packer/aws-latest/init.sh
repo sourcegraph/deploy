fd3929dd0fef2120cb8bbdf26e13568a122cc48f
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2024-09-12 14:08:35 -0400

    update AWS latest AMI

--- packer/aws-latest/init.sh
+++ packer/aws-latest/init.sh
@@ -15,10 +15,10 @@ KUBECONFIG_FILE='/etc/rancher/k3s/k3s.yaml'
 ###############################################################################
 # If running as root, deescalate
 if [ $UID -eq 0 ]; then
-    cd /home/ec2-user
-    chown ec2-user "$0" # /var/lib/cloud/instance/scripts/part-001
-    exec su ec2-user "$0" -- "$@"
-    # nothing will be executed beyond here (exec replaces the running process)
+  cd /home/ec2-user
+  chown ec2-user "$0" # /var/lib/cloud/instance/scripts/part-001
+  exec su ec2-user "$0" -- "$@"
+  # nothing will be executed beyond here (exec replaces the running process)
 fi
 
 ###############################################################################
@@ -38,8 +38,8 @@ git clone $SOURCEGRAPH_DEPLOY_REPO_URL
 # Format (if necessary) and mount the EBS volume
 device_fs=$(lsblk $EBS_VOLUME_DEVICE_NAME --noheadings --output fsType)
 if [ "$device_fs" == "" ]; then
-    sudo mkfs -t xfs $EBS_VOLUME_DEVICE_NAME
-    sudo xfs_admin -L /mnt/data $EBS_VOLUME_DEVICE_NAME
+  sudo mkfs -t xfs $EBS_VOLUME_DEVICE_NAME
+  sudo xfs_admin -L /mnt/data $EBS_VOLUME_DEVICE_NAME
 fi
 sudo mkdir -p /mnt/data
 sudo mount $EBS_VOLUME_DEVICE_NAME /mnt/data
@@ -92,12 +92,12 @@ sudo ln -s /mnt/data/storage /var/lib/rancher/k3s/storage
 ###############################################################################
 # Install k3s (Kubernetes single-machine deployment)
 ###############################################################################
-curl -sfL https://get.k3s.io | K3S_TOKEN=none sh -s - \
-    --node-name sourcegraph-0 \
-    --write-kubeconfig-mode 644 \
-    --cluster-cidr 10.10.0.0/16 \
-    --kubelet-arg containerd=/run/k3s/containerd/containerd.sock \
-    --etcd-expose-metrics true
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.31.0+k3s1 K3S_TOKEN=none sh -s - \
+  --node-name sourcegraph-0 \
+  --write-kubeconfig-mode 644 \
+  --cluster-cidr 10.10.0.0/16 \
+  --kubelet-arg containerd=/run/k3s/containerd/containerd.sock \
+  --etcd-expose-metrics true
 
 # Confirm k3s and kubectl are up and running
 sleep 5 && k3s kubectl get node
@@ -127,3 +127,4 @@ helm --kubeconfig $KUBECONFIG_FILE repo add sourcegraph https://helm.sourcegraph
 sleep 5
 sudo systemctl disable k3s
 sudo systemctl stop k3s
+
--- packer/aws-latest/install.sh
+++ packer/aws-latest/install.sh
@@ -12,8 +12,8 @@ LOCAL_BIN_PATH='/usr/local/bin'
 # Prepare the system
 ###############################################################################
 if [ -f /mnt/data/.sourcegraph-version ]; then
-    sleep 25 && bash $DEPLOY_PATH/reboot.sh
-    exit 0
+  sleep 25 && bash $DEPLOY_PATH/reboot.sh
+  exit 0
 fi
 
 ###############################################################################
@@ -23,23 +23,23 @@ fi
 ###############################################################################
 # If running as root, deescalate
 if [ $UID -eq 0 ]; then
-    chown ec2-user "$0" # /var/lib/cloud/instance/scripts/part-001
-    exec su ec2-user "$0" -- "$@"
-    # nothing will be executed beyond here (exec replaces the running process)
+  chown ec2-user "$0" # /var/lib/cloud/instance/scripts/part-001
+  exec su ec2-user "$0" -- "$@"
+  # nothing will be executed beyond here (exec replaces the running process)
 fi
 
 # Reset the containerd state if k3s is not starting
 # NOTE: Cluster data will NOT be deleted
 if ! sudo systemctl status k3s.service | grep -q 'active (running)'; then
-    # Stop all of the K3s containers and reset the containerd state
-    sudo sh $LOCAL_BIN_PATH/k3s-killall.sh
-    # Remove leftovers TLS certs and cred
-    sudo rm -rf $RANCHER_SERVER_PATH/cred/ $RANCHER_SERVER_PATH/tls/
-    # Enable k3s in this cluster and start the unit now
-    sudo systemctl enable --now k3s
+  # Stop all of the K3s containers and reset the containerd state
+  sudo sh $LOCAL_BIN_PATH/k3s-killall.sh
+  # Remove leftovers TLS certs and cred
+  sudo rm -rf $RANCHER_SERVER_PATH/cred/ $RANCHER_SERVER_PATH/tls/
+  # Enable k3s in this cluster and start the unit now
+  sudo systemctl enable --now k3s
 else
-    # Delete any existing ingress from old instances before restarting k3s
-    $LOCAL_BIN_PATH/kubectl --kubeconfig $KUBECONFIG_FILE delete ingress sourcegraph-ingress
+  # Delete any existing ingress from old instances before restarting k3s
+  $LOCAL_BIN_PATH/kubectl --kubeconfig $KUBECONFIG_FILE delete ingress sourcegraph-ingress
 fi
 sudo systemctl restart k3s
 sleep 30
@@ -53,11 +53,11 @@ cp override."$SOURCEGRAPH_SIZE".yaml override.yaml
 # Update information of available charts from Sourcegraph chart repository
 attempt=1
 while [ "$SOURCEGRAPH_VERSION" == "" ]; do
-    $LOCAL_BIN_PATH/helm --kubeconfig=$KUBECONFIG_FILE repo update
-    SOURCEGRAPH_VERSION=$($LOCAL_BIN_PATH/helm inspect chart sourcegraph/sourcegraph | grep version: | sed -r 's/version: (.*)/\1/')
-    attempt=$((attempt + 1))
-    if [ $attempt -eq 6 ]; then exit; fi
-    sleep 10
+  $LOCAL_BIN_PATH/helm --kubeconfig=$KUBECONFIG_FILE repo update
+  SOURCEGRAPH_VERSION=$($LOCAL_BIN_PATH/helm inspect chart sourcegraph/sourcegraph | grep version: | sed -r 's/version: (.*)/\1/')
+  attempt=$((attempt + 1))
+  if [ $attempt -eq 6 ]; then exit; fi
+  sleep 10
 done
 $LOCAL_BIN_PATH/helm --kubeconfig $KUBECONFIG_FILE pull --version "$SOURCEGRAPH_VERSION" sourcegraph/sourcegraph
 mv ./sourcegraph-"$SOURCEGRAPH_VERSION".tgz ./sourcegraph-charts.tgz
