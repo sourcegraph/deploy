8fc8f87a2e75af84b137a2b931f0a493ff24a81b
Marc LeBlanc <marc.leblanc@sourcegraph.com>
2025-03-25 22:07:25 -0600

    Add some newlines to make warning stand out

--- install/reboot.sh
+++ install/reboot.sh
@@ -50,10 +50,12 @@ function check_pod_statuses() {
   $KUBECTL_GET_ALL_CMD
 
   # If any pods are not running, then call them out specifically with a warning
-  PODS_NOT_RUNNING=$($KUBECTL_GET_PODS_CMD | grep -v -e Running -e Completed -e NAMESPACE -c)
-  if [[ $PODS_NOT_RUNNING -ne 0 ]]; then
-    log "WARNING: $PODS_NOT_RUNNING pods not running:"
+  COUNT_OF_PODS_NOT_RUNNING=$($KUBECTL_GET_PODS_CMD | grep -v -e Running -e Completed -e NAMESPACE -c)
+  if [[ $COUNT_OF_PODS_NOT_RUNNING -ne 0 ]]; then
+    printf "\n\n"
+    log "WARNING: $COUNT_OF_PODS_NOT_RUNNING pods not running:"
     $KUBECTL_GET_PODS_CMD | grep -v -e Running -e Completed
+    printf "\n\n"
   fi
 }
 
